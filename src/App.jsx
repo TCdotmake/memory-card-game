e04344be4e819a69fdc8f07e92b7cba77d060658
Tommy Chen <tommyjchen@hotmail.com>
2023-12-30 23:39:43 -0500

    implemented fetching of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 
+async function getData() {
+  const response = await fetch(
+    "https://api.scryfall.com/cards/search?include_extras=true&include_variations=true&order=set&q=e%3Awho&unique=prints"
+  );
+  const data = await response.json();
+  return await data;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [deck, setdeck] = useState([]);
+  useEffect(() => {
+    getData().then((data) => {
+      console.log(data.data);
+    });
+  }, []);
 
   return <></>;
 }
