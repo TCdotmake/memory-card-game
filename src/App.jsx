6e5eabe54a59f03030354c1741de90417fb26901
Tommy Chen <tommyjchen@hotmail.com>
2023-12-31 19:44:52 -0500

    working out logic for drawing 10 random cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
-
+/** @jsx jsx */
+import { css } from "@emotion/react";
 import "./App.css";
+import _ from "lodash";
 
 async function getData() {
   const response = await fetch(
@@ -11,14 +13,52 @@ async function getData() {
 }
 
 function App() {
+  const [data, setdata] = useState([]);
+  const [index, setindex] = useState([]);
+  const [pool, setpool] = useState([]);
   const [deck, setdeck] = useState([]);
+  const [step, setstep] = useState(1);
+  const [range, setrange] = useState(10);
+  const [gamesize, setgamesize] = useState(10);
   useEffect(() => {
-    getData().then((data) => {
-      console.log(data.data);
+    getData().then((result) => {
+      setdata(structuredClone(result.data));
     });
   }, []);
 
-  return <></>;
+  useEffect(() => {
+    let arr = [];
+    let i = 0;
+    if (data.length > 0) {
+      while (i < data.length) {
+        arr.push(i);
+        i += 1;
+      }
+      setindex(_.shuffle(arr));
+    }
+  }, [data]);
+
+  const handleShuffle = () => {
+    if (index.length > range && pool.length == 0) {
+      setpool(index.slice(0, range));
+      setdeck(index.slice(0, range));
+    }
+  };
+
+  const loading = "Loading...";
+
+  return (
+    <>
+      <button onClick={handleShuffle}>shuffle</button>
+      <div>
+        {(deck.length > 0 &&
+          deck.map((n) => {
+            return <button key={n}>{n}</button>;
+          })) ||
+          loading}
+      </div>
+    </>
+  );
 }
 
 export default App;
