bfa28195611b4fc4d0874bf185ef1dbdfdbe0b02
Tommy Chen <tommyjchen@hotmail.com>
2024-01-20 12:50:14 -0500

    installed framer motion, emotion and lodash, preparing to move files over

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { motion } from "framer-motion";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import _ from "lodash";
+const ecss = css`
+  color: red;
+`;
+
+let arr = [];
+for (let i = 0; i < 20; i++) {
+  arr.push(i);
+}
+
+arr = _.shuffle(arr);
+console.log(arr);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <motion.img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 2 }}
+          />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <motion.h1
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 2 }}
+        css={ecss}
+      >
+        Vite + React
+      </motion.h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +58,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
